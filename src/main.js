33463bc86ffec2fa71d363dc8bb3c1d3a57bc989
john <joao_bortotti@hotmail.com>
2020-04-12 18:31:14 -0300

    rework

--- src/main.js
+++ src/main.js
@@ -1,28 +1,31 @@
-document.getElementById("calcular").addEventListener("click", function(){
 
-  document.getElementById("erro").style.visibility="hidden"
-  var horasDiarias = parseInt(document.getElementById("valor-1").value);
-  var diasEfetivos = parseInt(document.getElementById("valor-2").value);
-  var diasFerias = parseInt(document.getElementById("valor-3").value);
-  var valorProjeto = parseInt(document.getElementById("valor-4").value);
+const button = document.getElementById("calcular")
+
+const resultadoHtml = document.getElementById("resultado")
+
+button.addEventListener('click', function() {
+  const horasDiarias = parseInt(document.getElementById("horasDiarias").value)
+  const diasEfetivos = parseInt(document.getElementById("diasEfetivos").value)
+  const diasFerias = parseInt(document.getElementById("diasFerias").value)
+  const valorProjeto = parseInt(document.getElementById("valorProjeto").value)
+    
+  const equacao = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + 
+    ( ( diasFerias * diasEfetivos * horasDiarias ) )
+
+  const valorHora = parseInt(equacao).toFixed(2);
+
+  if (isNaN(valorHora)) {
+    resultadoHtml.innerHTML = "Valor inválido"
+  } else {
+    resultadoHtml.innerHTML = `R$ ${valorHora}`
+     
+  }
   
-  if (isNaN(horasDiarias)) {
-      document.getElementById("erro").style.visibility="visible" 
-  }
-  else if (isNaN(diasEfetivos)) {
-      document.getElementById("erro").style.visibility="visible" 
-  }
-  else if(isNaN(diasFerias)) {
-      document.getElementById("erro").style.visibility="visible" 
-  }
-  else if(isNaN(valorProjeto)) {
-      document.getElementById("erro").style.visibility="visible" 
-  }
-  else {
-      const result = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
-      const valorHora = parseInt(result).toFixed(2);
-      document.getElementById("resultado").innerHTML = `R$ ${valorHora} p/ hora`;
-  }
-});
+
+})
+
+    
+    
+
 
 
